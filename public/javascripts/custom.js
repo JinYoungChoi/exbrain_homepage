fcb4648c2e6d0872ab36419236c124ca831d7688
JinYoungChoi <jkchoi0830@gmail.com>
2015-04-22 17:05:57 +0900

    initial project version

---
+++ public/javascripts/custom.js
@@ -0,0 +1,11 @@
+
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
